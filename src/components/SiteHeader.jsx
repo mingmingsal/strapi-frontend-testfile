c511b94eaae8c4d714e21fcaed54dc90c8849169
mingmingsal <miguellorenzosalvador@gmail.com>
2024-08-20 17:08:10 +0800

    rename header

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -2,7 +2,7 @@ import {Link} from 'react-router-dom'
 const SiteHeader = () => {
   return (
     <div className='site-header'>
-        <Link to='/'><h1>Ninja Reviews</h1></Link>
+        <Link to='/'><h1>Ming Reviews Games</h1></Link>
     </div>
   )
 }
